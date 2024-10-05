b9b4b5e566d9182078ced6917e9ddad2e04d1fa7
tether <sushant.spanwar@gmail.com>
2024-10-06 00:17:29 +0530

    Add array for sidebar

--- components/sidenav.tsx
+++ components/sidenav.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { SidebarContent } from "./sidebarContent";
 
+const data = [
+  {
+    header: "Getting started",
+    listItems: ["Introduction", "Installation", "Examples", "Gallary"],
+  },
+  {
+    header: "Components",
+    listItems: [
+      "Accordion",
+      "Alert",
+      "Alert Dialog",
+      "Aspect Ratio",
+      "Avatar",
+      "Badge",
+      "Breadcrumb",
+      "Button",
+      "Calendar",
+      "Card",
+      "Carousel",
+    ],
+  },
+];
+
 export const Sidenav = () => {
   return (
     <div className="container py-24 px-12 space-y-10 text-xl">
-      <SidebarContent header="Getting started" />
-      <SidebarContent header="Components" />
+      <SidebarContent data={data} />
     </div>
   );
 };
