caaacec29ae724e6ce2f2b20579b46182ab419b4
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-10-06 00:36:15 +0530

    stash

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,14 +1,26 @@
 import LoginButton from "@/components/auth/LoginButton";
+import { MainSection } from "@/components/mainSection";
 import { Navbar } from "@/components/navbar";
 import { Sidenav } from "@/components/sidenav";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { Wrapper } from "@/components/wrapper";
 
 export default function Home() {
   return (
     <Wrapper>
       <Navbar />
-      <Sidenav />
+      <div className="flex py-24 px-12 gap-16">
+        <div className="">
+          <Sidenav />
+        </div>
+        <div>
+          <Separator orientation="vertical" className="" />
+        </div>
+        <div className="flex-1">
+          <MainSection />
+        </div>
+      </div>
     </Wrapper>
   );
 }
---
+++ components/mainSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const MainSection = () => {
+  return <div className="py-24 h-full"></div>;
+};
