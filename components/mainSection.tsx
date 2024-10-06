cdaf4cb343cafdcfd47af02e84e1d9818cdd8a2a
tether <sushant.spanwar@gmail.com>
2024-10-06 12:31:57 +0530

    Initial code setup

--- components/mainSection.tsx
+++ components/mainSection.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import CodeSection from "./mainSection/codeSection";
 
 interface MainSectionProps {
   header: string;
@@ -15,11 +16,15 @@ interface MainSectionProps {
 
 export const MainSection = ({ header, description }: MainSectionProps) => {
   return (
-    <div className="h-full">
-      <div className="text-4xl font-semibold tracking-tight">{header}</div>
-      <div className="mt-3 text-white/50">{description}</div>
+    <div className="h-full space-y-10">
       <div>
-        
+        {/* intro section */}
+        <div className="text-4xl font-semibold tracking-tight">{header}</div>
+        <div className="mt-3 text-white/50">{description}</div>
+      </div>
+      <div>
+        {/* code section */}
+        <CodeSection />
       </div>
     </div>
   );
---
+++ components/mainSection/codeSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CodeSection = () => {
+  return <div>codeSection</div>;
+};
+
+export default CodeSection;
